ce181edb79465c50a39302661d7474ecda9db625
GeorgeFlood <mrgeorgeflood@gmail.com>
2022-10-02 15:05:53 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const background = document.querySelector(".background-container--day");
+
+// random quote generator
+
+// refresh button
+
+//navigation location
+
+// create new Intl time format.
+
+// create the clock
+
+// if it's > 18:00pm change class to night background && change message to good evening.
+
+// if it's > 12:00pm change message to good morning
+
+// a show more/less button than will add or remove a class showing extra information.

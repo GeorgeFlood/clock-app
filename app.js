360327d459de88a31003161fdc0bf52e0cf14100
GeorgeFlood <mrgeorgeflood@gmail.com>
2022-10-02 17:57:17 +0100

    quote generator

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
-const background = document.querySelector(".background-container--day");
+const background = document.querySelector(`.background-container--${"day"}`);
+const quote = document.querySelector("#quote");
+const quoteAuthor = document.querySelector("#author");
+const quoteRefresh = document.querySelector("#quote-refresh");
+
+console.log(background);
 
 // random quote generator
+const generateQuote = function (data) {
+  const randomQuote = data[Math.floor(Math.random() * data.length)];
+  const { text, author } = randomQuote;
+  quote.textContent = `"${text}"`;
+  quoteAuthor.textContent = author;
+};
 
-// refresh button
+const api = function () {
+  fetch("https://type.fit/api/quotes")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      generateQuote(data);
+    });
+};
 
+api();
+
+quoteRefresh.addEventListener("click", api);
 //navigation location
 
 // create new Intl time format.
